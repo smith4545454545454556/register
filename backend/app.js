aa2800bf1eff5e9b2a8da9618996ae43238bc496
smith4545454545454556 <smith.maharjan1233@gmail.com>
2024-11-25 19:34:34 +0545

    options

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,8 @@ const corsOptions = {
 const app = express()
 app.use(express.json())
 app.use(cors(corsOptions))
+app.options("*", cors(corsOptions));
+
 app.get("/", (req, res) => {
     res.send("hello")
 })
