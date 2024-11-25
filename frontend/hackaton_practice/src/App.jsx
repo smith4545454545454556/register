e3a8c4013c422f91a5f580f7db6b348771dd1a5c
smith4545454545454556 <smith.maharjan1233@gmail.com>
2024-11-25 17:43:18 +0545

    changes

--- frontend/hackaton_practice/src/App.jsx
+++ frontend/hackaton_practice/src/App.jsx
@@ -3,8 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
-import Login from './pages/login'
 import Register from './pages/register'
+import Home from './pages/Home'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Login />}
+        <Route path='/' element={<Home />}
         ></Route>
         <Route path='/signup' element={<Register />}></Route>
 
