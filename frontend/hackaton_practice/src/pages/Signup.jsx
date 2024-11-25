9da9bb328187bf5a876e1ff10245983ba450b05f
smith4545454545454556 <smith.maharjan1233@gmail.com>
2024-11-25 17:51:45 +0545

    again

--- frontend/hackaton_practice/src/pages/Signup.jsx
+++ frontend/hackaton_practice/src/pages/Signup.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import RegisterForm from '../components/registerForm'
+import Form from '../components/Form'
 
 const Signup = () => {
     return (
         <>
-            <RegisterForm />
+            <Form />
 
         </>)
 }
