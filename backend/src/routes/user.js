781869f02afe29f93e4668e5abb762cac4290ae1
smith4545454545454556 <smith.maharjan1233@gmail.com>
2024-11-25 18:54:51 +0545

    ..

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -4,6 +4,6 @@ import upload from "../multer/multer.js";
 export const router = Router()
 router.post("/register", upload.single("profile"), UserController.register)
 router.post("/login", UserController.login)
-router.get("/login", (res, req) => {
+router.get("/login", (req, res) => {
     res.send("register")
 })
